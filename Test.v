68ec6880dc898e942466199d8ff258cba7a23b63
Ranjan Yadav <61552167+ranjan-yadav@users.noreply.github.com>
2020-07-09 08:27:22 +0530

    Add files via upload

---
+++ Test.v
@@ -0,0 +1,191 @@
+`timescale 1ns / 1ps
+module Test;
+
+	// Inputs
+	reg high_frequency;
+	reg [7:0] input_pattern;
+	reg clk;
+
+	// Outputs
+	wire Output0;
+	wire Output1;
+	wire Output2;
+	wire Output3;
+	wire Output4;
+	wire Output5;
+	wire Output6;
+	wire Output7;
+
+	// Instantiate the Unit Under Test (UUT)
+	name uut (
+		.high_frequency(high_frequency), 
+		.clk(clk),
+		.Output0(Output0), 
+		.Output1(Output1), 
+		.Output2(Output2), 
+		.Output3(Output3), 
+		.Output4(Output4), 
+		.Output5(Output5), 
+		.Output6(Output6), 
+		.Output7(Output7), 
+		.input_pattern(input_pattern)
+	);
+
+
+initial begin high_frequency=0; end
+always #1 high_frequency=~high_frequency;
+
+initial begin clk=0; end
+always #10 clk=~clk;
+      
+initial
+begin
+
+//print M pattern
+input_pattern=8'b11111111;    // HOW TO TYPE ANY LETTER, SO LET'S TAKE "M"
+#20                           //
+input_pattern=8'b00000010;    //  LSB #     # S0 input at 1 clock is 8'b1111111;
+#20                           //      ##   ## S0 input at 2 clock is 8'b0000010;
+input_pattern=8'b00000100;    //      # # # # S0 input at 3 clock is 8'b0000100;
+#20                           //      #  #  # S0 input at 4 clock is 8'b0001000;
+input_pattern=8'b00001000;    //      #     # S0 input at 5 clock is 8'b0000100;
+#20                           //      #     # S0 input at 6 clock is 8'b0000010;
+input_pattern=8'b00000100;    //      #     # S0 input at 7 clock is 8'b1111111;
+#20                           //  MSB #     # S0 input at 8 clock is 8'b0000000;// for space
+input_pattern=8'b00000010;    //
+#20                           //
+input_pattern=8'b11111111;    //
+#20                           //
+input_pattern=8'b00000000;    //
+
+//space
+#20
+input_pattern=8'b00000000;
+
+//print A pattern
+input_pattern=8'b11111000;
+#20
+input_pattern=8'b00001100;
+#20
+input_pattern=8'b00001010;
+#20
+input_pattern=8'b00001001;
+#20
+input_pattern=8'b00001010;
+#20
+input_pattern=8'b00001100;
+#20
+input_pattern=8'b11111000;
+#20
+input_pattern=8'b00000000;
+
+//space
+#20
+input_pattern=8'b00000000;
+
+//print T
+input_pattern=8'b00000001;
+#20
+input_pattern=8'b00000001;
+#20
+input_pattern=8'b00000001;
+#20
+input_pattern=8'b11111111;
+#20
+input_pattern=8'b00000001;
+#20
+input_pattern=8'b00000001;
+#20
+input_pattern=8'b00000001;
+#20
+input_pattern=8'b00000000;
+
+//space
+#20
+input_pattern=8'b00000000;
+
+//print A pattern
+input_pattern=8'b11111000;
+#20
+input_pattern=8'b00001100;
+#20
+input_pattern=8'b00001010;
+#20
+input_pattern=8'b00001001;
+#20
+input_pattern=8'b00001010;
+#20
+input_pattern=8'b00001100;
+#20
+input_pattern=8'b11111000;
+#20
+input_pattern=8'b00000000;
+
+//space
+#20
+input_pattern=8'b00000000;
+
+//print M pattern
+input_pattern=8'b11111111;
+#20
+input_pattern=8'b00000010;
+#20
+input_pattern=8'b00000100;
+#20
+input_pattern=8'b00001000;
+#20
+input_pattern=8'b00000100;
+#20
+input_pattern=8'b00000010;
+#20
+input_pattern=8'b11111111;
+#20
+input_pattern=8'b00000000;
+
+//space
+#20
+input_pattern=8'b00000000;
+
+//print A pattern
+input_pattern=8'b11111000;
+#20
+input_pattern=8'b00001100;
+#20
+input_pattern=8'b00001010;
+#20
+input_pattern=8'b00001001;
+#20
+input_pattern=8'b00001010;
+#20
+input_pattern=8'b00001100;
+#20
+input_pattern=8'b11111000;
+#20
+input_pattern=8'b00000000;
+
+//space
+#20
+input_pattern=8'b00000000;
+
+//print T
+input_pattern=8'b10000001;
+#20
+input_pattern=8'b10000001;
+#20
+input_pattern=8'b10000001;
+#20
+input_pattern=8'b11111111;
+#20
+input_pattern=8'b10000001;
+#20
+input_pattern=8'b10000001;
+#20
+input_pattern=8'b10000001;
+#20
+input_pattern=8'b00000000;
+
+//space
+#20
+input_pattern=8'b00000000;
+end
+endmodule
---
+++ name.v
@@ -0,0 +1,106 @@
+`timescale 1ns / 1ps
+module name(
+  input high_frequency,
+  input clk,
+  output reg Output0, Output1, Output2, Output3, Output4, Output5, Output6, Output7,
+  input [7:0] input_pattern
+    );
+
+parameter IDLE=0;
+parameter HIGH_FREQUENCY=1; 
+
+reg state0=0;	reg state1=0;	reg state2=0;	reg state3=0;
+reg state4=0;	reg state5=0;	reg state6=0;	reg state7=0;
+	 
+initial 
+begin
+Output0<=1'b0; Output0<=1'b0; Output0<=1'b0; Output0<=1'b0;
+Output0<=1'b0; Output0<=1'b0; Output0<=1'b0; Output0<=1'b0; 
+end
+
+always @(posedge high_frequency)
+	begin
+			if(state0==HIGH_FREQUENCY)
+			Output0<=~Output0;
+			else if(state0==IDLE)
+			Output0<=0;
+			
+		   if(state1==HIGH_FREQUENCY)
+			Output1<=~Output1;
+			else if(state1==IDLE)
+			Output1<=0;
+			
+		   if(state2==HIGH_FREQUENCY)
+			Output2<=~Output2;
+			else if(state2==IDLE)
+			Output2<=0;
+			
+			if(state3==HIGH_FREQUENCY)
+			Output3<=~Output3;
+			else if(state3==IDLE)
+			Output3<=0;
+			
+			if(state4==HIGH_FREQUENCY)
+			Output4<=~Output4;
+			else if(state4==IDLE)
+			Output4<=0;
+			
+			if(state5==HIGH_FREQUENCY)
+			Output5<=~Output5;
+			else if(state5==IDLE)
+			Output5<=0;
+			
+			if(state6==HIGH_FREQUENCY)
+			Output6<=~Output6;
+			else if(state6==IDLE)
+			Output6<=0;
+			
+			if(state7==HIGH_FREQUENCY)
+			Output7<=~Output7;
+			else if(state7==IDLE)
+			Output7<=0;
+end
+
+always @(posedge clk)
+    begin 
+	if(input_pattern[0]==0)
+			state0<=IDLE;
+	else
+			state0<=HIGH_FREQUENCY;
+			
+	if(input_pattern[1]==0)
+			state1<=IDLE;
+	else
+			state1<=HIGH_FREQUENCY;
+			
+	if(input_pattern[2]==0)
+			state2<=IDLE;
+	else
+			state2<=HIGH_FREQUENCY;
+			
+	if(input_pattern[3]==0)
+			state3<=IDLE;
+	else
+			state3<=HIGH_FREQUENCY;
+			
+	if(input_pattern[4]==0)
+			state4<=IDLE;
+	else
+			state4<=HIGH_FREQUENCY;
+			
+	if(input_pattern[5]==0)
+			state5<=IDLE;
+	else
+			state5<=HIGH_FREQUENCY;
+			
+	if(input_pattern[6]==0)
+			state6<=IDLE;
+	else
+			state6<=HIGH_FREQUENCY;
+			
+	if(input_pattern[7]==0)
+			state7<=IDLE;
+	else
+			state7<=HIGH_FREQUENCY;
+end
+endmodule
